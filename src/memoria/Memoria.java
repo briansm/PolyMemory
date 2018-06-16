b3fac6dcc0d6884d4f15554908b91195f7bf799b
Brian Morales <briansmor09@gmail.com>
2018-06-15 21:26:38 -0600

    Interfaz inicial del proyecto

---
+++ src/memoria/Memoria.java
@@ -0,0 +1,13 @@
+
+package memoria;
+
+
+public class Memoria {
+
+    
+    public static void main(String[] args) {
+        Ventana_Analisis ventana_analisis = new Ventana_Analisis();
+        ventana_analisis.show();
+    }
+    
+}
