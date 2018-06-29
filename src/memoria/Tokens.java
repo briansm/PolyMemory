a996cf5889becf807521ef4497e2bc92c9b808cc
Brian Morales <briansmor09@gmail.com>
2018-06-29 17:11:26 -0600

    Agregada clase Token

---
+++ src/memoria/Tokens.java
@@ -0,0 +1,18 @@
+
+package memoria;
+
+
+public class Tokens {
+     String token="";
+     
+     
+     public Tokens(String token){
+         this.token=token;
+     
+     }
+        
+        public  String getTok(){
+            return token;
+        
+        }
+}
