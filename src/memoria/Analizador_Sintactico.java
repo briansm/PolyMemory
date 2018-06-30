bddda431ce3c1ba3ba107e41470732e3c9be45d7
Brian Morales <briansmor09@gmail.com>
2018-06-29 20:43:47 -0600

    Programado hasta produccion JuegoP

--- src/memoria/Analizador_Sintactico.java
+++ src/memoria/Analizador_Sintactico.java
@@ -52,14 +52,107 @@ public class Analizador_Sintactico {
         }
         
         public void Modul(){
-    
+            Sub();
+            ModulP();
     
         }
         
         public void ModulP(){
-    
+             Sub();
+             ModulP();
     
         }
+        
+        public void Sub(){
+            if(valor.getTok().equals("Token_[")){
+                parea("Token_[");
+                M();
+                parea("Token_]");
+            }
+    
+        }
+        
+        public void M(){
+            if(valor.getTok().equals("Token_Juego")){
+                parea("Token_Juego");
+                parea("Token_]");
+                MJ();
+                parea("Token_[");
+                parea("Token_End-Juego");
+            }else if(valor.getTok().equals("Token_Usuario")){
+                parea("Token_Usuario");
+                parea("Token_]");
+                MU();
+                parea("Token_[");
+                parea("Token_End-Usuario");
+            }else if(valor.getTok().equals("Token_Sonido")){
+                parea("Token_Sonido");
+                parea("Token_]");
+                MS();
+                parea("Token_[");
+                parea("Token_End-Sonido");
+            }else if(valor.getTok().equals("Token_Carta")){
+                parea("Token_Carta");
+                parea("Token_]");
+                MC();
+                parea("Token_[");
+                parea("Token_End-Carta");
+            }
+        }
+        
+        public void MJ(){
+            Juego();
+            MJP();
+        }
+        
+        public void Juego(){
+            if(valor.getTok().equals("Token_[")){
+                parea("Token_[");
+                JuegoP();
+                parea("Token_]");
+            }
+        }
+        
+        public void JuegoP(){
+            if(valor.getTok().equals("Token_Nivel")){
+                parea("Token_Nivel");
+                parea("Token_]");
+                SN();
+                parea("Token_[");
+                parea("Token_End-Nivel");
+            }else if(valor.getTok().equals("Token_Tiempo")){
+                parea("Token_Tiempo");
+                parea("Token_]");
+                ST();
+                parea("Token_[");
+                parea("Token_End-Tiempo");
+            }
+        }
+        
+        public void SN(){
+        
+        }
+        
+        public void ST(){
+        
+        }
+        
+        public void MJP(){
+        
+        }
+        
+        public void MU(){
+        
+        }
+        public void MS(){
+        
+        }
+        public void MC(){
+        
+        }
+        
+        
+        
         
         public void parea(String p){
             if(!p.equals(valor.getTok())){
